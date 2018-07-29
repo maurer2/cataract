5488dd0607db2dcf305243f4a8f58ec7a9b4d7c5
Martin <mr.mmaurer+work@gmail.com>
2018-07-29 14:51:43 +0200

    Fixed compiled js

---
+++ src/node/MorseSymbols.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var MorseSymbols;
+(function (MorseSymbols) {
+    MorseSymbols["DOT"] = ".";
+    MorseSymbols["DASH"] = "-";
+    // SPACE = ' ',
+})(MorseSymbols = exports.MorseSymbols || (exports.MorseSymbols = {}));
